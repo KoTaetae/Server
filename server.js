18a36a59a0aa2dd818a93f34432366a5f770f1da
KoTaehyung <komalgupta17503@gmail.com>
2022-08-24 00:09:24 +0530

    Completed node-server part

--- server.js
+++ server.js
@@ -2,10 +2,84 @@ const http = require('http');
 
 const port = 8081;
 
-http.createServer((request, response) => {
-response.writeHead(200,{'Content-type':'text/html'});
-response.write("<h1>Hello from server</h1>");
-response.end();
-}).listen(port, () => {
+const toDoList = ["Play Badminton", "Watch Dramas"];
+
+http
+.createServer((req, res) => {
+const {method, url} =  req ;
+
+if(url === "/todos") {
+if(method === "GET"){
+    res.writeHead(200, {"Content-type" : "text/html"});
+    res.write(toDoList.toString());
+    } 
+    
+else if (method === "POST"){
+       let body = "";
+       req
+       
+       .on("error", (err) => {
+       console.error(err)
+       })
+       .on("data", (chunk) => {
+          body += chunk;
+       })
+     .on("end", () => {
+        body = JSON.parse(body);
+        let newToDo = toDoList;
+        newToDo.push(body.item);
+        console.log(newToDo);
+        res.writeHead(201);
+       });
+}
+
+else if(method === "DELETE"){
+  let body = '';
+  req.on('error', () => {  
+    console.log(error);
+  })
+
+.on('data', (chunk) => {
+ body+= chunk;
+})
+.on('end', () => {
+    body = JSON.parse(body);
+    let deleteThis = body.item;
+  
+    for(let i=0; i< toDoList.length; i++){
+        if(toDoList[i] === deleteThis){
+            toDoList.splice(i, 1);
+            break;
+        }
+    }
+/*
+
+toDoList.find((element, index) => {
+    if(element === deleteThis){
+        toDoList.splice(index, 1);
+    }
+});
+
+*/
+res.writeHead(204);
+
+});
+
+}
+
+else{
+    res.writeHead(501);
+}
+}else{
+    res.writeHead(404);
+}
+
+res.end();
+})
+
+
+
+
+.listen(port, () => {
     console.log(`Node.js server started on ${port}`);
 });
